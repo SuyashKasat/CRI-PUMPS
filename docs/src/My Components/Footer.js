0441acb6e5aa20b69ba26c1a9a3d1f5e5d38ab7b
SuyashKasat <82160562+SuyashKasat@users.noreply.github.com>
2021-07-18 11:52:29 +0530

    Add files via upload

---
+++ docs/src/My Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export const Footer = () => {
+    let style={
+    backgroundColor:"red"
+    
+    }
+    return (
+        <div className="text-center" style={style}>
+        <h1> hello</h1>
+        </div>
+    )
+}
+
